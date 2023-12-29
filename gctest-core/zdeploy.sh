bb93e69563b9a6720c9a75f782d0cd7580f54442
cogo152 <gokay.comert@icloud.com>
2023-12-29 20:41:32 +0100

    Release 1.1.0 - backward compability

--- gctest-core/zdeploy.sh
+++ gctest-core/zdeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-package_cloud push cogo152/gproject/ubuntu/jammy package/gctest-core_1.0.8_arm.deb
-package_cloud push cogo152/gproject/ubuntu/jammy package/gctest-core_1.0.8_arm64.deb
-package_cloud push cogo152/gproject/ubuntu/jammy package/gctest-core_1.0.8_amd64.deb
+package_cloud push cogo152/gproject/ubuntu/jammy package/gctest-core_1.1.0_arm.deb
+package_cloud push cogo152/gproject/ubuntu/jammy package/gctest-core_1.1.0_arm64.deb
+package_cloud push cogo152/gproject/ubuntu/jammy package/gctest-core_1.1.0_amd64.deb
 
-package_cloud push cogo152/gproject/el/8 package/gctest-core-1.0.8-1.x86_64.rpm
+package_cloud push cogo152/gproject/el/8 package/gctest-core-1.1.0-1.x86_64.rpm
