5b0245a93432367d30bbfc7c8eff2a4d06d66ff8
cogo152 <gokay.comert@icloud.com>
2024-01-04 22:01:24 +0100

    desable rpm deployment

--- gctest-core/zdeploy.sh
+++ gctest-core/zdeploy.sh
@@ -4,4 +4,4 @@ package_cloud push cogo152/gproject/ubuntu/jammy package/gctest-core_1.1.0_arm.d
 package_cloud push cogo152/gproject/ubuntu/jammy package/gctest-core_1.1.0_arm64.deb
 package_cloud push cogo152/gproject/ubuntu/jammy package/gctest-core_1.1.0_amd64.deb
 
-package_cloud push cogo152/gproject/el/8 package/gctest-core-1.1.0-1.x86_64.rpm
+#package_cloud push cogo152/gproject/el/8 package/gctest-core-1.1.0-1.x86_64.rpm
